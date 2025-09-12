446917cf682e464a2f4efc281b05bd83ea993c6e
LewieJ08 <lewiej08@gmail.com>
2025-09-12 23:37:13 +0100

    feat: use mwLogger

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mwLogger = require("./middleware/logger");
+
 const app = express();
 const port = 3000;
+
+app.use(mwLogger);
 
 app.get("/api", (req, res) => {
     res.json({message: "api index"});
