2f87ff314d2c1e149e47b2ba0abe39969f401831
LewieJ08 <lewiej08@gmail.com>
2025-09-14 14:28:48 +0100

    feat: import and use errorhandler middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mwLogger = require("./middleware/logger");
+const mwErrorhandler = require("./middleware/errorhander");
 
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(mwLogger);
+app.use(mwErrorhandler);
 
 app.get("/api", (req, res) => {
     res.json({message: "api index"});
