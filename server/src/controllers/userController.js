ce1480ed2ceb8a336b064b6c35eb8f0ac67face3
LewieJ08 <lewiej08@gmail.com>
2025-09-16 00:48:50 +0100

    feat: add password hashing to registerUser func

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt")
 
 const getUsers = async (req, res, next) => {
     try {
@@ -15,15 +16,25 @@ const getUsers = async (req, res, next) => {
 
 const registerUser = async (req, res, next) => {
     try {
-        const user = new User(req.body);
-        user.save();
+        const {username, password} = req.body;
+        const hashedPassword = bcrypt.hash(password, 10);
+
+        const user = new User({username: username, password: hashedPassword});
+        await user.save();
         res.status(201).json({
             success: true,
             message: `User: ${req.body.username} registered successfully`,
             data: user
         });
     } catch(error) {
-        next(error);
+        if (error.code === 11000) {
+            res.status(400).json({
+                success: false,
+                error: `User with username: '${username}' already exists`
+            });
+        } else {
+            next(error);
+        }
     }
 }
 
