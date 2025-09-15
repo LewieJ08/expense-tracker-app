b6a71a66a0e321e6c01f9e527675040aaec3b4cb
LewieJ08 <lewiej08@gmail.com>
2025-09-15 10:17:15 +0100

    feat: add User model

---
+++ server/src/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        default: null
+    }
+});
+
+module.exports = new mongoose.model("User", userSchema);
