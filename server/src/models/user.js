fac45d8cab427cbe3d988082ca15d067640b4188
LewieJ08 <lewiej08@gmail.com>
2025-09-16 00:36:07 +0100

    refactor: update model password property

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    hashedPassword: {
+    password: {
         type: String,
         required: true
     },
