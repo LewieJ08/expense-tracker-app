d4965b403d92540da38bb4d40387298db582c7cc
LewieJ08 <lewiej08@gmail.com>
2025-09-15 09:43:52 +0100

    feat: import and call connectDB func

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./config/db")
 const mwLogger = require("./middleware/logger");
 const mwErrorhandler = require("./middleware/errorHandler");
 const userRoutes = require("./routes/userRoutes");
@@ -12,6 +13,8 @@ app.use("/users", userRoutes);
 
 app.use(mwErrorhandler);
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-});
+connectDB().then(
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+    })
+);
