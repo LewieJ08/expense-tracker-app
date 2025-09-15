493a48eff3084058e06c10a97875eb68988b7eb2
LewieJ08 <lewiej08@gmail.com>
2025-09-16 00:40:13 +0100

    feat: use json middleware

--- server/src/server.js
+++ server/src/server.js
@@ -8,8 +8,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(mwLogger);
-app.use("/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(mwErrorhandler);
 
