eb0e26038606f2a96b013a277bfdc89ad6615140
LewieJ08 <lewiej08@gmail.com>
2025-09-19 09:04:39 +0100

    feat: add Login component

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function loginUser(e) {
+        e.preventDefault();
+        
+        try {
+            const response = await fetch("/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({username: username, password: password})
+            }); 
+
+            if (!response.ok) {
+                throw new Error("Response Error");
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+
+        } catch(error) {
+            throw error;
+        }
+    }
+    return (
+        <div className="loginForm">
+            <form onSubmit={loginUser}>
+                <input 
+                    type="text" 
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input 
+                    type="text" 
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
