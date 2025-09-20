1554445a29ae84a703e9e60e508a313f962b69cb
LewieJ08 <lewiej08@gmail.com>
2025-09-20 20:53:37 +0100

    feat: add header for logged in user

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,24 @@
-function Header(props) {
+function Header() {
+    const user = localStorage.getItem("user");
+
+    if (!user) {
+        return (
+            <div className="header">
+                <h1>Expense Tracker</h1>
+                <div className="headerButtons">
+                    <button className="loginButton">Register</button>
+                    <button className="loginButton">Log In</button>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="header">
             <h1>Expense Tracker</h1>
-            <div className="loginButtons">
-                <button className="loginButton">Register</button>
-                <button className="loginButton">Log In</button>
+            <div className="headerButtons">
+                <p>{user.username}</p>
+                <button className="loginButton">Log Out</button>
             </div>
         </div>
     )
