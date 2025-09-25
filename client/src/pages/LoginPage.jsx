83eed0a49fe1b58481dafb37c18d6f2086eee364
LewieJ08 <lewiej08@gmail.com>
2025-09-25 15:37:47 +0100

    feat: add pages/

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function loginUser(e) {
+        e.preventDefault();
+        
+        try {
+            const response = await fetch("/api/users/login", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({username: username, password: password})
+            }); 
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const resBody = await response.json();
+            localStorage.setItem("user", JSON.stringify(resBody.data));
+
+            location.reload();
+
+        } catch(error) {
+            console.log(error.message);
+        }
+    }
+    return (
+        <div className="loginForm">
+            <form onSubmit={loginUser}>
+                <input 
+                    type="text" 
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
