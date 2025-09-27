a881cca36d144cb44ec3ab11aba091ed4d70090e
LewieJ08 <lewiej08@gmail.com>
2025-09-27 01:13:45 +0100

    feat: pass props into Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,7 @@
 import userIcon from "../assets/userIcon.png"
 import { Link } from "react-router-dom";
 
-function Header() {
-    const storedUser = localStorage.getItem("user");
-    const user = storedUser ? JSON.parse(storedUser) : null;
-
+function Header(props) {
     async function logoutUser() {
 
         try {
@@ -29,7 +26,7 @@ function Header() {
         location.reload();
     }
 
-    if (!user) {
+    if (!props.user) {
         return (
             <div className="header">
                 <h1>Expense Tracker</h1>
@@ -51,7 +48,7 @@ function Header() {
             <div className="headerButtons">
                 <div className="userInfo">
                     <img src={userIcon} className="userIcon"/>
-                    <p id="headerUsername">{user.username}</p>
+                    <p id="headerUsername">{props.user.username}</p>
                 </div>
                 <button onClick={logoutUser} className="headerButton">Log Out</button>
             </div>
