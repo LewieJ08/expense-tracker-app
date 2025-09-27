ce213240a8449aab03e6a94207292090d73cd69e
LewieJ08 <lewiej08@gmail.com>
2025-09-27 01:16:07 +0100

    refactor: get user from localStorage within HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import Header from "../components/Header"
 
 function Home() {
+    const storedUser = localStorage.getItem("user");
+    const user = storedUser ? JSON.parse(storedUser) : null;
+    
     return (
-        <Header />
+        <Header user={user}/>
     )
 }
 
