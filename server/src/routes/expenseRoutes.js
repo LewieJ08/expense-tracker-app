b7aefa5b943fbb2a254b5a8d0426f7113cc64044
LewieJ08 <lewiej08@gmail.com>
2025-09-29 17:14:37 +0100

    feat: setup expense routes

---
+++ server/src/routes/expenseRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const expenseController = require("../controllers/expenseController");
+
+const router = express.Router();
+
+router.route("/")
+    .get(expenseController.getExpenses);
