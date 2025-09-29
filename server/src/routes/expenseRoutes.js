bb5d84b20955e2532fa1085b08284bf9ea641254
LewieJ08 <lewiej08@gmail.com>
2025-09-30 00:50:47 +0100

    feat: wrap mwAuth around expenseRoutes

--- server/src/routes/expenseRoutes.js
+++ server/src/routes/expenseRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const expenseController = require("../controllers/expenseController");
+const mwAuth = require("../middleware/auth");
 
 const router = express.Router();
+router.use(mwAuth);
 
 router.route("/")
     .get(expenseController.getExpenses)
