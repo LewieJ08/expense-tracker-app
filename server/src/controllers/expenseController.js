d0438bc7bfda1efbf4f92af01d42de88704c30ba
LewieJ08 <lewiej08@gmail.com>
2025-09-30 01:43:28 +0100

    feat: add createExpense func

--- server/src/controllers/expenseController.js
+++ server/src/controllers/expenseController.js
@@ -1,16 +1,43 @@
 const Expense = require("../models/Expense");
 
 const getExpenses = async (req, res, next) => {
-    const expenses = await Expense.find({_userId: req.user.user_id})
     try {
+        const expenses = await Expense.find({_userId: req.user.user_id});
         res.status(200).json({
             success: true,
             message: "success placeholder message",
             data: expenses
-        })
+        });
     } catch(error) {
         next(error);
     }
 }
 
-module.exports = {getExpenses, }
+const createExpense = async (req, res, next) => {
+    const {title, cost, category} = req.body;
+    try {
+        const expense = new Expense({
+            _userId: req.user._id,
+            title: title,
+            cost: cost,
+            category: category
+        });
+
+        await expense.save();
+        res.status(201).json({
+            success: true,
+            message: `Expense '${title}' created successfully`
+        });
+    } catch(error) {
+        if (error.code === 11000) {
+            res.status(400).json({
+                success: false,
+                error: `${title} already exists, please update the title`
+            });
+        } else {
+            next(error);
+        }
+    }
+}
+
+module.exports = {getExpenses, createExpense};
