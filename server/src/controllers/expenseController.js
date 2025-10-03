9f0174af68549268df4d9d8f5a689df8f05cd4b2
LewieJ08 <lewiej08@gmail.com>
2025-10-03 11:02:33 +0100

    refactor: remove testing console log

--- server/src/controllers/expenseController.js
+++ server/src/controllers/expenseController.js
@@ -3,7 +3,7 @@ const Expense = require("../models/Expense");
 const getExpenses = async (req, res, next) => {
     try {
         const expenses = await Expense.find({_userId: req.user._id});
-        console.log(expenses)
+                  
         res.status(200).json({
             success: true,
             message: `Expenses for ${req.user.username} fetched successfully`,
