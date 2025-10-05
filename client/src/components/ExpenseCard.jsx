fc36ef68e7f6ec18782085ae27a31d465bd2000c
LewieJ08 <lewiej08@gmail.com>
2025-10-06 00:19:49 +0100

    add delete functionality to ExpenseCard

--- client/src/components/ExpenseCard.jsx
+++ client/src/components/ExpenseCard.jsx
@@ -1,17 +1,45 @@
+import { useNavigate } from "react-router-dom";
+
+const currencySymbols = {
+    "GBP": "£",
+    "USD": "$",
+    "EUR": "€",
+    "JPY": "¥"
+}
+
 function ExpenseCard(props) {
-    const currencySymbols = {
-        "GBP": "£",
-        "USD": "$",
-        "EUR": "€",
-        "JPY": "¥"
+    const navigate = useNavigate();
+    const currency = currencySymbols[props.user.currency];
+
+    async function deleteExpense() {
+        try {
+            const response = await fetch(`/api/expenses/${props.id}`, {
+                method: "DELETE",
+                headers: {Authorization: `Bearer ${props.user.token}`}
+            });
+
+            if (!response.ok) {
+                if (response.status === 404) {
+                    navigate("/404");
+                } else {
+                    throw new Error(`Response Status: ${response.status}`);
+                }
+            }
+
+            location.reload();
+
+        } catch(error) {
+            console.log(error.message);
+            throw error;
+        }
     }
-    const currency = currencySymbols[props.user.currency]
 
     return (
         <div className="expenseCard">
             <h3>{props.title}</h3>
             <p>{currency}{props.cost}</p>
             <p>{props.category}</p>
+            <button onClick={deleteExpense}>Delete</button>
         </div>
     )
 }
