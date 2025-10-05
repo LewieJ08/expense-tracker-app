821ada762f288e1485558a2d061ab4ea82e0f43a
LewieJ08 <lewiej08@gmail.com>
2025-10-05 22:23:59 +0100

    feat: add urlencoded middleware

--- server/src/server.js
+++ server/src/server.js
@@ -16,6 +16,7 @@ const corsOptions = {
 };
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors(corsOptions));
 app.use(mwLogger);
 app.use("/api/users", userRoutes);
