9dcf878825fafb76798e321facfa3ff2540d9a8a
LewieJ08 <lewiej08@gmail.com>
2025-10-05 19:09:44 +0100

    feat: add user currency symbols to card

--- client/src/components/ExpenseCard.jsx
+++ client/src/components/ExpenseCard.jsx
@@ -1,8 +1,16 @@
 function ExpenseCard(props) {
+    const currencySymbols = {
+        "GBP": "£",
+        "USD": "$",
+        "EUR": "€",
+        "JPY": "¥"
+    }
+    const currency = currencySymbols[props.user.currency]
+
     return (
         <div className="expenseCard">
             <h3>{props.title}</h3>
-            <p>{props.cost}</p>
+            <p>{currency}{props.cost}</p>
             <p>{props.category}</p>
         </div>
     )
